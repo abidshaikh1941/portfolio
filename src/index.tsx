e6841d0150907e928b664186a20ee99da594fe01
Abid Shaikh <abid@kristal.ai>
2025-06-26 02:45:40 +0530

    portfolio project - 2

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Index from './pages/Index';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <index />
+  </React.StrictMode>
+);
